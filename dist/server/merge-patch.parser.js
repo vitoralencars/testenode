1fd678a8a1a4e94ef54615496f9d354df637cb8d
sv0021 <vitor.siqueira@bancodaycoval.com.br>
2018-09-17 11:49:25 -0300

    Correção merge parser

--- dist/server/merge-patch.parser.js
+++ dist/server/merge-patch.parser.js
@@ -5,10 +5,10 @@ exports.mergePatchBodyParser = function (req, resp, next) {
     if (req.getContentType() === mpContentType && req.method === 'PATCH') {
         try {
             req.body = JSON.parse(req.body);
-            return next();
         }
         catch (e) {
             return next(new Error("Conte\u00FAdo inv\u00E1lido: " + e.message));
         }
     }
+    return next();
 };
--- server/merge-patch.parser.ts
+++ server/merge-patch.parser.ts
@@ -2,13 +2,13 @@ import * as restify from 'restify'
 
 const mpContentType = 'application/merge-patch+json'
 
-export const mergePatchBodyParser = (req: restify.Request, resp: restify.Response, next)=>{
+export const mergePatchBodyParser = (req: restify.Request, resp: restify.Response, next: restify.Next)=>{
     if(req.getContentType() === mpContentType && req.method === 'PATCH'){
         try{
             req.body = JSON.parse(req.body)
-            return next()
         }catch(e){
             return next(new Error(`Conteúdo inválido: ${e.message}`))
         }
     }
+    return next()
 }
